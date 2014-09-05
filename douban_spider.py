80f0dc9f534c64cbc2e6d386dcb05308844159ba
Leon.R.Sine <leonsin112358@gmail.com>
2014-09-05 18:01:08 +0800

    refactor BookSearch

--- douban_spider.py
+++ douban_spider.py
@@ -2,13 +2,15 @@ import urllib2
 import sys
 import re
 
-
-book_search_response = urllib2.urlopen('http://book.douban.com/subject_search?search_text=python&cat=1001')
-
-html = book_search_response.read()
-
-response_page = html.decode("utf-8")
-
+class BookSearch:
+	def getBookList(self):
+		book_search_response = urllib2.urlopen('http://book.douban.com/subject_search?search_text=python&cat=1001')
+		html = book_search_response.read()
+		response_page = html.decode("utf-8")
+		# book list in <li> tag
+		bookList = re.findall('<li class="subject-item">(.*?)</li>', response_page, re.S)
+		return bookList
+	
 """
 # python default env encode format is 'ascii'
 # reload sys module for UnicodeDecodeError
@@ -18,12 +20,15 @@ sys.setdefaultencoding('utf-8')
 with open('search_python_book.html','w+') as f:
 	f.write(html.decode("utf-8"))
 """
-# book list in <li> tag
-bookItems = re.findall('<li class="subject-item">(.*?)</li>', response_page, re.S)
 
-bookUrl = []
-bookTitle = []
-
-# 
-for item in bookItems
-	re.findall('<div class="info">(.*?)')
+if __name__ == '__main__':
+	bookSearch = BookSearch()
+	bookListInfo = bookSearch.getBookList()
+	print bookListInfo
+	bookURLList = []
+	if(bookListInfo):
+		print 'bookListInfo is not null %d' % len(bookListInfo)
+		for bookInfo in bookListInfo:
+			bookURL = re.match(r'http://book\.douban\.com/subject/\d+/', bookInfo)
+			bookURLList.append(bookURL)
+	print bookURLList
