4e720a2f3377536c2f05379afafc273412e6828b
Leon.R.Sine <leonsin112358@gmail.com>
2014-09-05 16:37:19 +0800

    add entity class book

---
+++ book.py
@@ -0,0 +1,56 @@
+class book(self):
+	""" A entity class : book"""
+	def setTitle(self.title):
+		self.title = title 
+	
+	def getTitle(self):
+		return self.title
+	
+	def setOriginalName(self.originalName):
+		self.originalName = originalName
+
+	def getOriginalName(self):
+		return self.originalName
+	
+	def setPublicHouse(self.publicHouse):
+		self.publicHouse = publicHouse 
+	
+	def getPublicHouse(self):
+		return self.publicHouse
+	
+	def setTranslator(self.translator):
+		self.translator = translator 
+	
+	def getTranslator(self):
+		return self.translator
+	
+	def setPublicTime(self.publicTime):
+		self.publicTime = publicTime 
+	
+	def getPublicTime(self):
+		return self.publicTime
+	
+	def setTotalPage(self.totalPage):
+		self.totalPage = totalPage 
+	
+	def getTotalPage(self):
+		return self.totalPage
+	
+	def setPrice(self.price):
+		self.price = price 
+	
+	def getPrice(self):
+		return self.price
+	
+	def setISBN(self.isbn):
+		self.isbn = isbn 
+	
+	def getISBN(self):
+		return self.isbn
+	
+	def setRating(self.rating):
+		self.rating = rating 
+	
+	def getRating(self):
+		return self.rating
+	
